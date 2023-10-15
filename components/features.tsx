a87bb117eb3c9eaefeee2eab2453ab6a06cdd0b0
Abdul Hadi <hadi2408466@gmail.com>
2023-10-15 12:28:14 +0500

    services added

--- components/features.tsx
+++ components/features.tsx
@@ -1,4 +1,9 @@
-import WebLogo from "../public/images/web_code.png";
+import WebLogo from "../public/images/coding.png";
+import AppLogo from "../public/images/app-dev.png";
+import DigiLogo from "../public/images/bullhorn.png";
+import SEO from "../public/images/seo.png";
+import UxLogo from "../public/images/design.png";
+import AD from "../public/images/advertising.png";
 import Image from "next/image";
 
 export default function Features() {
@@ -68,9 +73,8 @@ export default function Features() {
               </div>
               <h4 className="h4 mb-2 ">Web Developement</h4>
               <p className="text-lg text-gray-400 text-center">
-                Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                cupidatat.
+                Transforming ideas into sleek, functional, and user-friendly
+                web masterpieces.
               </p>
             </div>
 
@@ -81,37 +85,19 @@ export default function Features() {
               data-aos-delay="100"
               data-aos-anchor="[data-aos-id-blocks]"
             >
-              <svg
-                className="w-16 h-16 mb-4"
-                viewBox="0 0 64 64"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <circle
-                  className="fill-current text-purple-600"
-                  cx="32"
-                  cy="32"
-                  r="32"
+              <div className="bg-purple-600  rounded-full">
+                <Image
+                  src={AppLogo}
+                  alt="Web Logo"
+                  width={50}
+                  height={50}
+                  className="m-3"
                 />
-                <path
-                  className="stroke-current text-purple-100"
-                  strokeWidth="2"
-                  strokeLinecap="square"
-                  d="M21 23h22v18H21z"
-                  fill="none"
-                  fillRule="evenodd"
-                />
-                <path
-                  className="stroke-current text-purple-300"
-                  d="M26 28h12M26 32h12M26 36h5"
-                  strokeWidth="2"
-                  strokeLinecap="square"
-                />
-              </svg>
+              </div>
               <h4 className="h4 mb-2">App Development</h4>
               <p className="text-lg text-gray-400 text-center">
-                Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                cupidatat.
+                From concept to code, redefine excellence in mobile
+                app development.
               </p>
             </div>
 
@@ -122,48 +108,19 @@ export default function Features() {
               data-aos-delay="200"
               data-aos-anchor="[data-aos-id-blocks]"
             >
-              <svg
-                className="w-16 h-16 mb-4"
-                viewBox="0 0 64 64"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect
-                  className="fill-current text-purple-600"
-                  width="64"
-                  height="64"
-                  rx="32"
+              <div className="bg-purple-600  rounded-full">
+                <Image
+                  src={AD}
+                  alt="Web Logo"
+                  width={50}
+                  height={50}
+                  className="m-3"
                 />
-                <g
-                  transform="translate(21 21)"
-                  strokeLinecap="square"
-                  strokeWidth="2"
-                  fill="none"
-                  fillRule="evenodd"
-                >
-                  <ellipse
-                    className="stroke-current text-purple-300"
-                    cx="11"
-                    cy="11"
-                    rx="5.5"
-                    ry="11"
-                  />
-                  <path
-                    className="stroke-current text-purple-100"
-                    d="M11 0v22M0 11h22"
-                  />
-                  <circle
-                    className="stroke-current text-purple-100"
-                    cx="11"
-                    cy="11"
-                    r="11"
-                  />
-                </g>
-              </svg>
+              </div>
               <h4 className="h4 mb-2">Digital Marketing</h4>
               <p className="text-lg text-gray-400 text-center">
-                Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                cupidatat.
+                Transforming ideas into impactful stories that leave a
+                lasting impression.
               </p>
             </div>
 
@@ -174,86 +131,43 @@ export default function Features() {
               data-aos-delay="300"
               data-aos-anchor="[data-aos-id-blocks]"
             >
-              <svg
-                className="w-16 h-16 mb-4"
-                viewBox="0 0 64 64"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect
-                  className="fill-current text-purple-600"
-                  width="64"
-                  height="64"
-                  rx="32"
+              <div className="bg-purple-600  rounded-full">
+                <Image
+                  src={SEO}
+                  alt="Web Logo"
+                  width={50}
+                  height={50}
+                  className="m-3"
                 />
-                <g
-                  transform="translate(22 21)"
-                  strokeLinecap="square"
-                  strokeWidth="2"
-                  fill="none"
-                  fillRule="evenodd"
-                >
-                  <path
-                    className="stroke-current text-purple-100"
-                    d="M17 22v-6.3a8.97 8.97 0 003-6.569A9.1 9.1 0 0011.262 0 9 9 0 002 9v1l-2 5 2 1v4a2 2 0 002 2h4a5 5 0 005-5v-5"
-                  />
-                  <circle
-                    className="stroke-current text-purple-300"
-                    cx="13"
-                    cy="9"
-                    r="3"
-                  />
-                </g>
-              </svg>
-              <h4 className="h4 mb-2">SEO</h4>
+              </div>
+              <h4 className="h4 mb-2">SEO/Content writing</h4>
               <p className="text-lg text-gray-400 text-center">
-                Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                cupidatat.
+                From SEO mastery to social media magic, Your brand's online
+                journey starts here, with our dynamic marketing solutions
               </p>
             </div>
-
-            {/* 5th item */}
             <div
               className="relative flex flex-col items-center"
               data-aos="fade-up"
-              data-aos-delay="400"
+              data-aos-delay="300"
               data-aos-anchor="[data-aos-id-blocks]"
             >
-              <svg
-                className="w-16 h-16 mb-4"
-                viewBox="0 0 64 64"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect
-                  className="fill-current text-purple-600"
-                  width="64"
-                  height="64"
-                  rx="32"
+              <div className="bg-purple-600  rounded-full">
+                <Image
+                  src={DigiLogo}
+                  alt="Web Logo"
+                  width={50}
+                  height={50}
+                  className="m-3"
                 />
-                <g
-                  strokeLinecap="square"
-                  strokeWidth="2"
-                  fill="none"
-                  fillRule="evenodd"
-                >
-                  <path
-                    className="stroke-current text-purple-100"
-                    d="M29 42h10.229a2 2 0 001.912-1.412l2.769-9A2 2 0 0042 29h-7v-4c0-2.373-1.251-3.494-2.764-3.86a1.006 1.006 0 00-1.236.979V26l-5 6"
-                  />
-                  <path
-                    className="stroke-current text-purple-300"
-                    d="M22 30h4v12h-4z"
-                  />
-                </g>
-              </svg>
-              <h4 className="h4 mb-2">Content Writing</h4>
+              </div>
+              <h4 className="h4 mb-2">Social Media Management</h4>
               <p className="text-lg text-gray-400 text-center">
                 Duis aute irure dolor in reprehenderit in voluptate velit esse
                 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
                 cupidatat.
               </p>
             </div>
-
             {/* 6th item */}
             <div
               className="relative flex flex-col items-center"
@@ -261,45 +175,19 @@ export default function Features() {
               data-aos-delay="500"
               data-aos-anchor="[data-aos-id-blocks]"
             >
-              <svg
-                className="w-16 h-16 mb-4"
-                viewBox="0 0 64 64"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect
-                  className="fill-current text-purple-600"
-                  width="64"
-                  height="64"
-                  rx="32"
+              <div className="bg-purple-600  rounded-full">
+                <Image
+                  src={UxLogo}
+                  alt="Web Logo"
+                  width={50}
+                  height={50}
+                  className="m-3"
                 />
-                <g
-                  transform="translate(21 22)"
-                  strokeLinecap="square"
-                  strokeWidth="2"
-                  fill="none"
-                  fillRule="evenodd"
-                >
-                  <path
-                    className="stroke-current text-purple-300"
-                    d="M17 2V0M19.121 2.879l1.415-1.415M20 5h2M19.121 7.121l1.415 1.415M17 8v2M14.879 7.121l-1.415 1.415M14 5h-2M14.879 2.879l-1.415-1.415"
-                  />
-                  <circle
-                    className="stroke-current text-purple-300"
-                    cx="17"
-                    cy="5"
-                    r="3"
-                  />
-                  <path
-                    className="stroke-current text-purple-100"
-                    d="M8.86 1.18C3.8 1.988 0 5.6 0 10c0 5 4.9 9 11 9a10.55 10.55 0 003.1-.4L20 21l-.6-5.2a9.125 9.125 0 001.991-2.948"
-                  />
-                </g>
-              </svg>
+              </div>
               <h4 className="h4 mb-2">Graphic Design</h4>
               <p className="text-lg text-gray-400 text-center">
-                Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                cupidatat.
+                From concept to canvas, crafting designs that resonate with
+                your audience.
               </p>
             </div>
           </div>
