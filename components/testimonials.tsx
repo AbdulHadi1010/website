4404b96b831c081f3280db97ed6e16c8a8934cbe
Abdul Hadi <hadi2408466@gmail.com>
2024-01-16 22:24:14 +0500

    update

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -2,16 +2,32 @@
 import Image from "next/image";
 import React from "react";
 import TestimonialImage01 from "@/public/images/mainImg.jpg";
-import TestimonialImage02 from "@/public/images/testimonial-02.jpg";
-import TestimonialImage03 from "@/public/images/testimonial-03.jpg";
+import TestimonialImage02 from "@/public/images/projectImg.jpg";
 
 export default function Testimonials() {
-  const [items, setItems] = React.useState(["notarizr", "a", "b", "c", "d"]);
-  const [selected, setSelected] = React.useState([]);
-  const [position, setPosition] = React.useState(0);
+  const items = [
+    {
+      Name: "Notarizr",
+      ImagePath: TestimonialImage01,
+      Description:
+        "Dive into a virtual celebration of scientific wonders and engineering marvels at our university's Festival where innovation meets inspiration.",
+      Link: "http://www.scifestucp.com",
+    },
+    {
+      Name: "AI Mobile App",
+      ImagePath: TestimonialImage02,
+      Description:
+        "Explore a futuristic chatbot for instant AI-driven assistance on your mobile app, making interactions smarter and more efficient.",
+      Link: "",
+    },
+  ];
+
   return (
     <section>
-      <div className="max-w-6xl mx-auto px-4  sm:px-6">
+      <div
+        className="max-w-6xl mx-auto px-4  sm:px-6"
+        id="our-projects-section"
+      >
         <div className="py-12 md:py-20 border-t border-gray-800">
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-6 md:pb-20">
@@ -19,35 +35,29 @@ export default function Testimonials() {
             {/* <p className="text-xl text-gray-400">Coming Soon...</p> */}
           </div>
           <div className="no-scrollbar flex gap-6 max-w-full  overflow-auto">
-            {/* <ScrollMenu
-              LeftArrow={LeftArrow}
-              RightArrow={RightArrow}
-              transitionBehavior={"auto"}
-              transitionDuration={300}
-              
-            > */}
-            {items.map(({ id }) => (
+            {items.map(({ Name, ImagePath, Description, Link }, index: any) => (
               <div
-                className="flex flex-col flex-shrink-0 h-full p-6 mx-3  bg-gray-800"
+                className="flex flex-col flex-shrink-0 h-full p-6 mx-3 cursor-pointer bg-gray-800"
                 data-aos="fade-up"
-                key={id}
+                key={index}
+                onClick={() => {
+                  window.open(Link, "_blank");
+                }}
               >
                 <div className="relative inline-flex flex-col mb-4">
                   <Image
-                    // className="rounded-full"
-                    src={TestimonialImage01}
+                    src={ImagePath}
                     width={300}
                     height={200}
                     alt="Testimonial 01"
                   />
                 </div>
-                <blockquote className="text-lg w-48 text-gray-400 grow">
-                  A intuitive website for a university's proud occasion of
-                  Science & Engineering Festival
+                <div className="font-bold text-2xl my-0.5">{Name}</div>
+                <blockquote className="text-lg  w-64 text-gray-400 grow">
+                  {Description}
                 </blockquote>
               </div>
             ))}
-            {/* </ScrollMenu> */}
           </div>
         </div>
       </div>
